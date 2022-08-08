62d910ecc16ee69c77ed564bfaf8ad00e125039d
katerina <you-like-me@yandex.ru>
2022-08-08 23:28:06 +0300

    Add Task 003

---
+++ Task003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
