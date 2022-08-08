b2d32fa3b59ef9ca2bb820359bb8ca06ff5cda30
katerina <you-like-me@yandex.ru>
2022-08-08 20:25:14 +0300

    Add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+//int max = 0;
+
+if(a > b)
+{
+    Console.WriteLine("Максимальное число a");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Максимальное число b");
+    Console.WriteLine(b);
+}
